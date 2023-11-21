caca7c3b50b490ccbc659d0e18a13e2b075a1d70
Cíntia Arantes <114604292+cintiaarantes@users.noreply.github.com>
2023-11-21 15:53:08 -0300

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,18 @@
+//Desafio 01: Criar uma navegação em gavetas
+
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import Home from "../screens/Home";
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Início" component={Home} />
+            <Drawer.Screen name="Abas" component={TabNavigator} />
+        </Drawer.Navigator>
+    );
+};
+
+export default DrawerNavigator;
